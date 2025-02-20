a7aa1c742b78f23171dedf02f60f5b1a326571c1
Manish <156883335+Manish50518@users.noreply.github.com>
2025-02-20 22:53:55 +0530

    fixing error

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Amplify } from "aws-amplify";
-import awsExports from "./aws-exports.js";
 import "./index.css";
 import App from "./App.jsx";
 
-Amplify.configure(awsExports);
+// Amplify Configuration using Environment Variables
+Amplify.configure({
+  Auth: {
+    region: import.meta.env.VITE_AWS_PROJECT_REGION,
+    userPoolId: import.meta.env.VITE_AWS_USER_POOLS_ID,
+    userPoolWebClientId: import.meta.env.VITE_AWS_CLIENT_ID,
+    oauth: {
+      domain: import.meta.env.VITE_OAUTH_DOMAIN,
+      scope: ["email", "openid", "profile"],
+      redirectSignIn: import.meta.env.VITE_REDIRECT_SIGNIN,
+      redirectSignOut: import.meta.env.VITE_REDIRECT_SIGNOUT,
+      responseType: "code",
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
